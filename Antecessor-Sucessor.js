f30579a2ef392498c36ad6a60d09d8690ef8d602
XavierLimaTI <xavierlima.ti@gmail.com>
2023-08-10 11:08:48 -0300

    ex2pronto

---
+++ Antecessor-Sucessor.js
@@ -0,0 +1,7 @@
+window.alert("Seja bem vindo ao programa sequencial de números!");
+let numero1= parseInt(window.prompt("Digite um número"));
+
+var antecessor = numero1 - 1
+var sucessor = numero1 + 1
+
+window.alert(`O antecessor do número escolhido é: ${antecessor}, o número é: ${numero1} e seu sucessor é o número: ${sucessor}`)
